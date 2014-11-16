58435a3600c4262ca85d2bdcdb1fce283a8a919e
James Black <planiturthian@gmail.com>
2014-11-15 21:23:54 -0500

    Created initial tests for MSSQL DAO
    Created very basic tests for the MSSQL DAO.

---
+++ GiftCardMSSQLDAOTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using Newtonsoft.Json;
+using MyGiftCard;
+
+namespace GiftCertProTest
+{
+    [TestClass]
+    public class GiftCardMSSQLDAOTest
+    {
+        GiftCardMSSQLDAO test = null;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            test = new GiftCardMSSQLDAO();
+            test = test.Init();
+        }
+
+        [TestMethod]
+        public void TestRetrieveClients()
+        {
+            var results = test.retrieveClients();
+            if (results.Count == 0)
+                new AssertFailedException("Nothing was returned from the test");
+        }
+
+        [TestMethod]
+        public void TestRetrievePendingOrders()
+        {
+            var results = test.retrievePendingOrders("testcompany", DateTime.Now.AddDays(-30), DateTime.Now);
+            if (results.Count == 0)
+                new AssertFailedException("Nothing was returned from the test");
+        }
+        [TestMethod]
+        public void TestRetrievePendingOrdersWithFilteredName()
+        {
+            var results = test.retrievePendingOrders("testcompany", DateTime.Now.AddDays(-30), DateTime.Now, "testrecipient");
+            if (results.Count == 0)
+                new AssertFailedException("Nothing was returned from the test");
+        }
+    }
+}
